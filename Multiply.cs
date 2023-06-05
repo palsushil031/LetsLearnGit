64b69b208f1e616c064ad1131ddff81948e27226
Sushil Pal <palsushil031@gmail.com>
2023-06-05 14:41:41 +0530

    added multiply feature

---
+++ Multiply.cs
@@ -0,0 +1,10 @@
+public class Multiply{
+
+    public static void main(String[] arg){
+        int a = 10;
+        int b = 20;
+
+        Console.WriteLine("The sum is " + a*b);
+        Console.ReadLine();
+    }
+}
